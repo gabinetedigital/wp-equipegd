95060ba3be75436bb316133e1e7d2b0ab4c8ddcd
Leo Andrade <leo-andrade@procergs.rs.gov.br>
2012-10-01 09:18:02 -0300

    Adaptado o estilo do widget ao layout e feito a paginação da equipe

--- wp-equipegd-widget.php
+++ wp-equipegd-widget.php
@@ -39,45 +39,138 @@ class ListaEquipeGDWidget extends WP_Widget
   function widget($args, $instance)
   {
     extract($args, EXTR_SKIP);
- 
+
     $args_query_post = '';
 	$txtreturn		 = '';
+	$taxonomy		 = 'grupo_equipegd';
 	$titulo			 = empty($instance['titulo']) ? ' ' : apply_filters('widget_titulo', $instance['titulo']);
     $colunas		 = $instance['colunas'];
     $custom_post 	 = 'equipegd_equipe';
     $args_query_post .= "post_type=" . $custom_post;
+	$js = new ListaEquipeGDWidget();
+	
+	//$args_query_post .= "tax_query='terms=gabinetedigital'";
+//			'field' => 'slug',
+//			'terms' => 'bob''";
+	
 	$args_query_post .= "&orderby=meta_value_num&meta_key=wp_equipegd_ordem";
 	$args_query_post .= "&order=ASC";
 
 	query_posts($args_query_post);
 
 	$txtreturn .= "<div class='equipe'>";
-	$txtreturn .= "<h3>".$titulo."</h3>";
-	$txtreturn .= "<ul class='thumbnails'>";
-
+	$arr = array();
+	$i = 0;
 	if (have_posts()) : 
 		while (have_posts()) : the_post(); 
+			$nome  = get_the_title();
 			$cargo = get_post_meta(get_the_ID(),'wp_equipegd_cargo', true);
 			$ordem = get_post_meta(get_the_ID(),'wp_equipegd_ordem', true);
-			$grupo = get_the_term_list(get_the_ID(), 'grupo_equipegd' );
+			$grupo = wp_get_post_terms(get_the_ID(), $taxonomy, array('fields' => 'names'));
+			$grupo = $grupo[0];
 
-			$txtreturn .= "<li class='span".$colunas."'>";
-			$txtreturn .= "<div class='thumbnail'>";
-			$txtreturn .= "<h4>".get_the_title()."</h4>";
-			$txtreturn .= "<h5>". $cargo . "</h5>";
-			$txtreturn .= "</div>";
-			$txtreturn .= "</li>";
-
+			$arr[$i] =  array("nome" => $nome, "cargo" => $cargo, "ordem" => $ordem, "grupo" => $grupo);
+			
+			$i++;
 		endwhile;
 	endif; 
 	wp_reset_query();
+
+	$array_ordenado = $js->ordenar_array($arr, 'grupo', SORT_ASC, 'ordem', SORT_ASC) or die('<br>ERROR!<br>');
+	
+/*	foreach ($arr as $key => $row) {   
+    	$filtro[$key]  = $row['grupo'];   
+    }
+    array_multisort($filtro, SORT_ASC, $arr);
+ */ 
+ 	$i = 0;
+	$cont=0;
+	$x = 0;
+	$nomegrupo = array();
+	while (list($key, $value) = each($array_ordenado)) {
+    	//$txtreturn .= "Key: $key; Value: $value[grupo]<br />\n";
+    	$grupo = substr($value[grupo], 1,strlen($value[grupo]));
+    	if ($cont == 0){
+    		$grupo_inic = $grupo;
+    	}
+    	if($grupo != $grupo_inic){
+    		$grupo_inic = $grupo;
+			$i = 0;
+			$txtreturn .= "</ul>";
+			$txtreturn .= "</div>";
+    	}
+    	if ($i == 0){
+			$x++;
+			$nomegrupo[$x] = $grupo;
+    		$txtreturn .= "<div id='equipe-grupo-$x'>";
+    		$txtreturn .= "<h3>".$titulo." - ".$grupo."</h3>";
+			$txtreturn .= "<ul class='thumbnails'>";
+    	}
+    	$txtreturn .= "<li class='span".$colunas."'>";
+		$txtreturn .= "<div class='thumbnail'>";
+		$txtreturn .= "<h4>$value[nome]</h4>";
+		//$txtreturn .= "<h5>".$value[ordem]." - ". substr($value[grupo], 1,strlen($value[grupo])) . " - ". $value[cargo] . "</h5>";
+		$txtreturn .= "<h5>". $value[cargo] . "</h5>";
+		$txtreturn .= "</div>";
+		$txtreturn .= "</li>";
+		
+		$cont++;
+		$i++;
+	}
 	
 	$txtreturn .= "</ul>";
+	$txtreturn .= "</div>";
+	$txtreturn .= "<input name='equipe-perpage' type='hidden' id='equipe-perpage' value='$x' />";
+	$txtreturn .= "<div>";
+	$i = 1;
+	foreach($nomegrupo as $g){
+		$txtreturn .= "<a class='equipe-grupo-$i equipe-grupo' data-index=$i>$g</a>";
+		$i++;
+	}
+	$txtreturn .= "</div>";
 	$txtreturn .= "</div>";
 	
 	echo $txtreturn;
   }
- 
+
+	function ordenar_array() { 
+		$n_parametros = func_num_args();  
+		if ($n_parametros<3 || $n_parametros%2!=1) {  
+			return false; 
+		} else {  
+			$arg_list = func_get_args(); 
+
+			if (!(is_array($arg_list[0]) && is_array(current($arg_list[0])))) { 
+				return false;  
+			} 
+
+			for ($i = 1; $i<$n_parametros; $i++) {  
+				if ($i%2!=0) { 
+					if (!array_key_exists($arg_list[$i], current($arg_list[0]))) { 
+						return false; 
+					} 
+				} else {  
+					if ($arg_list[$i]!=SORT_ASC && $arg_list[$i]!=SORT_DESC) { 
+						return false; 
+					} 
+				} 
+			} 
+			$array_salida = $arg_list[0]; 
+
+			$a_evaluar = "foreach (\$array_salida as \$fila){\n"; 
+			for ($i=1; $i<$n_parametros; $i+=2) { 
+  				$a_evaluar .= "  \$campo{$i}[] = \$fila['$arg_list[$i]'];\n"; 
+			} 
+			$a_evaluar .= "}\n"; 
+			$a_evaluar .= "array_multisort(\n"; 
+			for ($i=1; $i<$n_parametros; $i+=2) {  
+				$a_evaluar .= "  \$campo{$i}, SORT_REGULAR, \$arg_list[".($i+1)."],\n"; 
+			} 
+			$a_evaluar .= "  \$array_salida);"; 
+			eval($a_evaluar); 
+			return $array_salida; 
+		} 
+	} 
 }
 add_action( 'widgets_init', create_function('', 'return register_widget("ListaEquipeGDWidget");') );
 
